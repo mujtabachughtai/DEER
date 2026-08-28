4d08936362c11562a83da7369abd8d1703fe4e8c
Muji Chughtai <132944447+mujtabachughtai@users.noreply.github.com>
2026-08-28 15:10:19 -0400

    Initial commit

---
+++ Study 2/Study2_DataCleaning_Secondary.R
@@ -0,0 +1,93 @@
+# Load in required libraries and data
+library(tidyverse)
+library(here)
+
+data <- read.csv(
+  here("Data", "Deidentified Raw Data", "Study2_Deidentified_Data.csv")
+)
+
+# Retain data from participants who passed attention checks
+data <- data %>%
+  filter(Attn_Check_Never == 5) %>%
+  filter(Attn_Check_Much == 2) %>%
+  select(-contains("Attn"))
+
+# Convert to long format
+TidyData <- data %>%
+  pivot_longer(
+    cols = contains(c("Experience", "Expression")),
+    names_to = "Type",
+    values_to = "Frequency"
+  ) %>%
+  pivot_longer(
+    cols = contains("Connection"),
+    names_to = "Type.2",
+    values_to = "Connection"
+  ) %>%
+  # Only keeping rows with matching contexts for connection and frequency
+  filter(substr(Type, 1, 6) == substr(Type.2, 1, 6))
+
+# Dividing `Type` variable into `Emotion` and `Relationship` and changing values
+# to have more meaningful labels
+TidyData <- TidyData %>%
+  mutate(Emotion = substr(Type, 1, 2)) %>%
+  mutate(Relationship = substr(Type, 4, 5)) %>%
+  mutate(Type = substr(Type, 7, length(Type))) %>%
+  mutate(Emotion = case_when(
+    Emotion == "X1" ~ "Respect",
+    Emotion == "X2" ~ "Pride",
+    Emotion == "X3" ~ "Guilt",
+    Emotion == "X4" ~ "Anger")) %>%
+  mutate(
+    Orientation = case_when(
+      Emotion == "Respect" ~ "Engaging",
+      Emotion == "Pride" ~ "Disengaging",
+      Emotion == "Guilt" ~ "Engaging",
+      Emotion == "Anger" ~ "Disengaging"
+    ),
+    Valence = case_when(
+      Emotion == "Respect" ~ "Positive",
+      Emotion == "Pride" ~ "Positive",
+      Emotion == "Guilt" ~ "Negative",
+      Emotion == "Anger" ~ "Negative"
+    )) %>%
+  mutate(Relationship = case_when(
+    Relationship == "RP" ~ "Romantic Partner",
+    Relationship == "CF" ~ "Close Friend",
+    Relationship == "CC" ~ "Classmate/Colleague",
+    Relationship == "ST" ~ "Stranger"
+  )) %>%
+  mutate(Relationship_Type = case_when(
+    Relationship == "Romantic Partner" ~ "Communal",
+    Relationship == "Close Friend" ~ "Communal",
+    Relationship == "Classmate/Colleague" ~ "Transactional",
+    Relationship == "Stranger" ~ "Transactional"
+  )) %>%
+  mutate(Frequency = 5 - Frequency) %>%
+  mutate(Connection = 3 - Connection) %>%
+  mutate(Age = as.numeric(Age))
+
+# Remove unnecessary columns and rearrange others. Sort by meaningful variables
+TidyData <- TidyData %>%
+  select(-matches("Type.2")) %>%
+  select(PID, Relationship_Type, Relationship, Orientation, Emotion, Valence, Type, Frequency, Connection, everything()) %>%
+  arrange(PID, Relationship_Type, Relationship, Orientation, Emotion)
+
+# Create df to combine with study 1
+Study2 <- TidyData %>%
+  mutate(PE = "Perception") %>%
+  select(PID, PE, Relationship_Type, Relationship, Orientation, Emotion, Valence, Type, Frequency, Connection)
+
+# Save main data to csv
+write.csv(
+  TidyData,
+  here("Data", "Analysis-Ready Data", "Study2_AnalysisData.csv"),
+  row.names = FALSE
+)
+
+# Save data for comparison to study 1 to csv
+write.csv(
+  Study2,
+  here("Data", "Analysis-Ready Data", "Study2_ComparisonData.csv"),
+  row.names = FALSE
+)
---
+++ Study 3/Study3_DataCleaning_Secondary.R
@@ -0,0 +1,94 @@
+# Load in required libraries and data
+library(tidyverse)
+library(here)
+
+data <- read.csv(
+  here("Data", "Deidentified Raw Data", "Study3_Deidentified_Data.csv")
+)
+
+# Retain data from participants who passed attention checks
+data <- data %>%
+  filter(Attn_Check_Never == 5) %>%
+  filter(Attn_Check_Much == 2) %>%
+  select(-contains("Attn"))
+
+# Convert to long format and match experience/expression with connection
+TidyData <- data %>%
+  pivot_longer(
+    cols = contains(c("Experience", "Expression")),
+    names_to = "Exp_Type",
+    values_to = "Frequency"
+  )%>%
+  pivot_longer(
+    cols = contains("Conn"),
+    names_to = "Type.2",
+    values_to = "Connection"
+  ) %>%
+  filter(substr(Exp_Type, 1, 7) == substr(Type.2, 1, 7))
+
+# Match with draw close or distance
+TidyData <- TidyData %>%
+  pivot_longer(
+    cols = contains("Dist"),
+    names_to = "Type.3",
+    values_to = "DrawClose"
+  ) %>%
+  filter(substr(Type.2, 1, 7) == substr(Type.3, 1, 7))
+
+# Match with affirming or undermining
+TidyData <- TidyData %>%
+  pivot_longer(
+    cols = contains("Afrm"),
+    names_to = "Type.4",
+    values_to = "Affirming"
+  ) %>%
+    filter(substr(Type.3, 1, 7) == substr(Type.4, 1, 7))
+
+# Match with willing
+TidyData <- TidyData %>%
+  pivot_longer(
+    cols = contains("Want"),
+    names_to = "Type.5",
+    values_to = "Willing"
+  ) %>%
+  filter(substr(Type.4, 1, 7) == substr(Type.5, 1, 7))
+
+# Keep matching recall and hypothetical
+TidyData <- TidyData %>%
+  filter(substr(Type.2, 14, 14) == substr(Type.3, 14, 14)) %>%
+  filter(substr(Type.3, 14, 14) == substr(Type.4, 14, 14))
+
+# Create predictor variables
+TidyData <- TidyData %>%
+  mutate(Relationship = substr(Exp_Type, 1,2)) %>%
+  mutate(Emotion = substr(Exp_Type, 4, 7)) %>%
+  mutate(Relationship_Type = case_when(Relationship == "RP" ~ "Communal",
+                                       Relationship == "CF" ~ "Communal",
+                                       Relationship == "CC" ~ "Transactional",
+                                       Relationship == "SS" ~ "Transactional"
+                                       )) %>%
+  mutate(Exp_Type = substr(Exp_Type, 9, length(Exp_Type))) %>%
+  mutate(Hyp_Recall = substr(Type.2, 14, length(Type.2))) %>%
+  mutate(Emotion = case_when(Emotion == "Hurt" ~ "Hurt",
+                              Emotion == "Joys" ~ "Joy",
+                              Emotion == "Love" ~ "Love",
+                              Emotion == "Sadn" ~ "Sadness")) %>%
+  select(-Type.2, -Type.3, -Type.4, -Type.5) %>%
+  arrange(PID, Relationship_Type, Relationship, Emotion, Exp_Type) %>%
+  select(PID, Relationship_Type, Relationship, Emotion, Exp_Type, Frequency,
+         Hyp_Recall, Connection, DrawClose, Affirming, Willing, everything())
+
+# Create dependent variables
+TidyData <- TidyData %>%
+  mutate(Frequency = 5 - Frequency) %>%
+  mutate(Connection = 3 - Connection) %>%
+  mutate(DrawClose = 3 - DrawClose) %>%
+  mutate(Affirming = 3 - Affirming) %>%
+  mutate(Willing = 3 - Willing)
+
+# Save data to csv
+write.csv(
+  TidyData,
+  here("Data", "Analysis-Ready Data", "Study3_AnalysisData.csv"),
+  row.names = FALSE
+)
